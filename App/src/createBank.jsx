0da8ab2455723b4ed0952951fb7fd9335bef713a
summerSunndy <mengyue8866@163.com>
2017-10-18 16:16:49 +0800

    createQbank

--- App/src/createBank.jsx
+++ App/src/createBank.jsx
@@ -1,6 +1,8 @@
 import editPage from './sass/editPage';
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import PropTypes from 'prop-types';
+import utils from './common/utils';
 
 // class createQbank extends Component
 class  Qbank extends React.Component{
@@ -8,14 +10,9 @@ class  Qbank extends React.Component{
         // 编辑、获取数据
         let xhr = new XMLHttpRequest(), temp;
         let qbankid = (temp = location.pathname.match(/\/(\w*)$/)) ? temp[1] : '';
-        xhr.open('get','http://localhost:3001/getqbank?qbankid='+qbankid, true);
-        xhr.withCredentials = true;
-        xhr.send(null);
-
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState === 4){
-                let response = JSON.parse(xhr.response);
-            }
+        let result = utils.ajax('get','http://localhost:3001/getqbank?qbankid='+qbankid);
+        for(let key in result.data){
+            this.state[key] = result.data[key]
         }
     }
     getState(){
@@ -23,15 +20,16 @@ class  Qbank extends React.Component{
     }
     finish_edit(){
 
-    }
-    finish_question(){
-
+        let data = new FormData();
+        utils.ajax('post','http://localhost:3001/updateQbank', data)
     }
     add_item(e){
         e.currentTarget.innsertBefore(this.refs.addCheckItem.cloneNode(true));
     }
-    validate(){
-
+    handleData(e,key){
+        this.setState({
+            [key]: e.currentTarget.value
+        });
     }
     addImage(e){
         let input = e.currentTarget;
@@ -60,7 +58,7 @@ class  Qbank extends React.Component{
                 <div className="nb_content">
                     <dl className="nb_create_item flex">
                         <dt>题库名</dt>
-                        <dd><input placeholder="请输入题库名称" type="text" maxLength="20"/></dd>
+                        <dd><input placeholder="请输入题库名称" value={this.state.qbank_name} onChange={(e)=>this.handleData(e, 'qbank_name')} type="text" maxLength="20"/></dd>
                     </dl>
                     <dl className="nb_create_item">
                         <dt>素材</dt>
@@ -78,7 +76,7 @@ class  Qbank extends React.Component{
                     </dl>
                     <dl className="nb_create_item">
                         <dt>答题规则</dt>
-                        <dd><textarea placeholder="请输入答题规则"></textarea></dd>
+                        <dd><textarea value={this.state.reply_rule}  onChange={(e)=>this.handleData(e, 'reply_rule') placeholder="请输入答题规则"></textarea></dd>
                     </dl>
                     <button className="nb_btn nb_btn_primary" onClick={this.finish_edit}>开始添加题目</button>
                     <button className="nb_btn nb_btn_primary" onClick={this.finish_edit}>完成</button>
@@ -88,4 +86,9 @@ class  Qbank extends React.Component{
         );
     }
 }
+// Qbank.PropTypes = {
+//     qbank_name: React.PropTypes.string.isRequired,
+//     reply_rule: React.PropTypes.string.isRequired,
+//     qbank_material_url: React.PropTypes.object.isRequired
+// }
 export default Qbank;
