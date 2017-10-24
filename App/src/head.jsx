319f5d1f2d5f469934e2486a8e85a562e3f5918c
summerSunndy <mengyue8866@163.com>
2017-10-24 18:03:28 +0800

    list header

--- App/src/head.jsx
+++ App/src/head.jsx
@@ -6,17 +6,17 @@ export default class  Header extends React.Component{
         super(props)
     }
     render(){
-        let {leftType, rightType, centerType, center} = this.props;
+        let {leftType, rightType, centerType, center, title} = this.props;
         const Left = (<div className="nb_top_left">{leftType === 'back' ? <a>返回</a> : <a>其他</a>}</div>)
         const Right = (<div className="nb_top_right">{rightType === 'menu' ? <a>菜单</a> : <a>创建</a>}</div>)
         const Center = (<div className="nb_top_center">{centerType === 'title'? <h3>{title}</h3>:<div className="nb_slot_wrap">其他</div>}</div>)
         return (
             <div className="nb_top_wrap">
                 <div className="nb_top_content">
-                    <Left/>
-                    <Center/>
-                    <Right/>
-                </div>
+                    {Left}
+                    {Center}
+                    {Right}
+                </div>Right
             </div>
         );
     }
