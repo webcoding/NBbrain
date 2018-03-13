f3e2d0df62e65e13ebb8fde7de747fc929289dc8
summerSunndy <mengyue8866@163.com>
2018-03-13 14:57:28 +0800

    animation

--- App/src/ant/index.jsx
+++ App/src/ant/index.jsx
@@ -8,19 +8,47 @@ export default class Ant extends React.Component{
       index: 1
     }
   }
-  start(e){
-    self = this.selfDom;
-    self.style.left=e.clientX - Math.floor(self.offsetWidth/2);
-    self.style.top=e.clientY - Math.floor(self.offsetHeight/2);
+  getPos(e){
+    return {
+      x: e.pageX,
+      y: e.pageY
+    }
+  }
+  init(){
     this.setState({
-      hasClass: true,
-      index: Math.floor(Math.random() * (7 - 1 + 1)) + 1
+      hasClass: false
     })
+  }
+  up(e){
+    this.dragging = false;
+  }
+  moving(e){
+    if(this.dragging){
+      let self = this.selfDom;
+      var x = this.getPos(e).x - this.offsetX;
+      var y = this.getPos(e).y - this.offsetY;
+      var width = document.documentElement.clientWidth - self.offsetWidth;
+      var height = document.documentElement.clientHeight - self.offsetHeight;
+
+      self.style.left = Math.min(Math.max(0, x), width);
+      self.style.top = Math.min(Math.max(0, y), height);
+      // self.style.transform = `translate(${x}px, ${y}px)`;
+      this.setState({
+        hasClass: true,
+        index: Math.floor(Math.random() * (7 - 1 + 1)) + 1
+      })
+    }
+  }
+  down(e){
+    let self = this.selfDom;
+    this.dragging = true
+    this.offsetX = this.getPos(e).x - self.offsetLeft;
+    this.offsetY = this.getPos(e).y - self.offsetTop;
   }
   render(){
     return (
-      <div className="container" onMouseDown={(e)=>{this.start(e)}}>
-        <i className={`it color-${this.state.index} ${this.state.hasClass ? 'hidden' : ''} `} ref={(input) => { this.selfDom = input; }}/>
+      <div className="container">
+        <i className={`it color-${this.state.index} ${this.state.hasClass ? 'hidden' : ''} `}  ref={(input) => { this.selfDom = input; }} onAnimationEnd={(e)=>{this.init(e);}} onMouseDown={(e)=>{this.down(e)}} onMouseMove={(e)=>{this.moving(e)}} onMouseUp={(e)=>{this.up(e);}}/>
       </div>
     )
   }
